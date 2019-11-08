fa586247ccab732091dec9f81ecf7060b901abfe
Rian Fadli <rianfadli11@gmail.com>
2019-11-08 14:49:52 +0700

    Display Post

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    public function getImageUrlAttribute($value)
+    {
+        $imageUrl = "";
+        if(! is_null($this->image))
+        {
+            $imagePath = public_path() . "/" . $this->image;
+            if(file_exists($imagePath)) $imageUrl = asset("/img/". $this->image);
+            
+        }
+
+        return $imageUrl;
+    }
 }
