d7cf244423653db58dd956573df4ca13c2b89afe
Rian Fadli <rianfadli11@gmail.com>
2019-11-12 13:05:34 +0700

    k

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -23,29 +23,44 @@ class BlogController extends Controller
                     ->latestFirst()
                     ->published()
                     ->simplePaginate($this->limit);
+
         return view("blog.index", compact('posts', 'categories'));
         //dd(\DB::getQuerylog());
     }
 
-    public function category($id)
+    public function category(Category $category)
     {
+
+        $categoryName = $category->title;
 
         $categories = Category::with(['posts' => function($query) {
             $query->published();
         }])->orderBy('title', 'asc')->get();
         // \DB::enableQueryLog();
         //$posts = Post::with('author')->orderBy('created_at', 'desc')->get();
-        $posts = Post::with('author')
-                    ->latestFirst()
-                    ->published()
-                    ->where('category_id', $id)
-                    ->simplePaginate($this->limit);
-        return view("blog.index", compact('posts', 'categories'));
+
+        // $posts = Post::with('author')
+        //             ->latestFirst()
+        //             ->published()
+        //             ->where('category_id', $id)
+        //             ->simplePaginate($this->limit);
+
+        $posts = $category->posts()
+                        ->with('author')
+                        ->latestFirst()
+                        ->published()
+                        ->simplePaginate($this->limit);
+
+        return view("blog.index", compact('posts', 'categories', 'categoryName'));
         //dd(\DB::getQuerylog());
     }
 
     public function show(Post $post)
     {
-        return view("blog.show", compact('post'));
+        $categories = Category::with(['posts' => function($query) {
+            $query->published();
+        }])->orderBy('title', 'asc')->get();
+
+        return view("blog.show", compact('post', 'categories'));
     }
 }
