c11959fc87611f9239f89d1bdfbc13f34b634902
Rian Fadli <rianfadli11@gmail.com>
2019-11-13 15:31:47 +0700

    add author bio and gravatar

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -27,6 +28,20 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class, 'author_id');
+    }
+
+    public function gravatar()
+    {
+        $email = $this->email;
+        $default = "https://cdn3.iconfinder.com/data/icons/avatar-20/100/avatar-55-512.png";
+        $size = 100;
+
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
+    }
+
+    public function getBioHtmlAttribute($value)
+    {
+        return $this->bio ? Markdown::convertToHtml(e($this->bio)) : NULL;
     }
 
     public function getRouteKeyName()
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,24 +17,28 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->truncate();
 
         //generate 3 users author
+        $faker = Factory::create();
         DB::table('users')->insert([
             [
                 'name' => "Jhon Doe",
                 'slug' => "jhon-doe",
                 'email' => "jhondoe@test.com",
-                'password' => bcrypt('secret')
+                'password' => bcrypt('secret'),
+                'bio' => $faker->text(rand(250, 300))
             ],
             [
                 'name' => "Rian Fadli",
                 'slug' => "rian-fadli",
                 'email' => "rian@test.com",
-                'password' => bcrypt('rian666')
+                'password' => bcrypt('rian666'),
+                'bio' => $faker->text(rand(250, 300))
             ],
             [
                 'name' => "elmaika",
                 'slug' => "elmaika",
                 'email' => "elmaika@test.com",
-                'password' => bcrypt('elmaika123')
+                'password' => bcrypt('elmaika123'),
+                'bio' => $faker->text(rand(250, 300))
             ],
         ]);
     }
