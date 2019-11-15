b78e101f6a22a50b27bceae1500a4e4a8b2ee129
Rian Fadli <rianfadli11@gmail.com>
2019-11-15 15:44:07 +0700

    page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main');
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 page-not-found">
+                <h2>Page Not Found</h2>
+                <p>Sorry, The page you're looking for couldn't be found</p>
+            </div>
+        </div>
+    </div>
+
+@endsection
