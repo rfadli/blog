3ab7c1e52d790b22c85d8240afcd6451a93280b7
Rian Fadli <rianfadli11@gmail.com>
2019-11-26 13:22:03 +0700

    add link to display data on trash

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -23,11 +23,22 @@ class BlogController extends BackendController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::with('category', 'author')->latest()->paginate($this->limit);
-        $postCount = Post::count();
-        return view("backend.blog.index", compact('posts', 'postCount'));
+        if(($status = $request->get('status')) && $status == 'trash')
+        {
+            $posts       = Post::onlyTrashed()->with('category', 'author')->latest()->paginate($this->limit);
+            $postCount   = Post::onlyTrashed()->count();
+            $onlyTrashed = TRUE;
+        }
+        else
+        {
+            $posts       = Post::with('category', 'author')->latest()->paginate($this->limit);
+            $postCount   = Post::count();
+            $onlyTrashed = FALSE;
+        }
+
+        return view("backend.blog.index", compact('posts', 'postCount', 'onlyTrashed'));
     }
 
     /**
@@ -141,6 +152,13 @@ class BlogController extends BackendController
         Post::findOrFail($id)->delete();
 
         return redirect('/backend/blog')->with('trash-message', ['Your Post Move To Trash', $id]);
+    }
+
+    public function forceDestroy($id)
+    {
+        Post::withTrashed()->findOrFail($id)->forceDelete();
+
+        return redirect('/backend/blog?status=trash')->with('message', 'Your post has been deleted successfuly');
     }
 
     public function restore($id)
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,4 +40,9 @@ Route::put('/backend/blog/restore/{blog}', [
     'as'    => 'backend.blog.restore'
 ]);
 
+Route::delete('/backend/blog/force-destroy/{blog}', [
+    'uses' => 'Backend\BlogController@ForceDestroy',
+    'as'    => 'backend.blog.force-destroy'
+]);
+
 Route::resource('/backend/blog', 'Backend\BlogController');
